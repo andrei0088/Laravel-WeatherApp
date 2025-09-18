7341f6c3e331e908ac78c4a97e33dc30697a0387
Andrei D2u <andreidavidoiu@gmail.com>
2025-09-18 19:27:09 +0200

    redy to push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>WeatherAPP</title>
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('pub/logo.png') }}">
+
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
